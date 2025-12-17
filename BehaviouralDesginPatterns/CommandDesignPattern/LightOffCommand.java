0de4f6999708244044d5e6243f20aa046ff2d69c
Sumanth Mamillapalli <mamillapallisumanth@kyndryl.com>
2025-12-17 10:37:44 +0530

    Command Pattern

---
+++ BehaviouralDesginPatterns/CommandDesignPattern/LightOffCommand.java
@@ -0,0 +1,12 @@
+package BehaviouralDesginPatterns.CommandDesignPattern;
+
+public class LightOffCommand implements Command{
+    private Light light;
+    public LightOffCommand(Light light){
+        this.light = light;
+    }
+    public void execute(){
+        light.turnOff();
+    }
+    
+}
---
+++ BehaviouralDesginPatterns/CommandDesignPattern/Main.java
@@ -0,0 +1,16 @@
+package BehaviouralDesginPatterns.CommandDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Light livingRoomLight = new Light();
+        Command turnOn = new LightOnCommand(livingRoomLight);
+        Command turnOff = new LightOffCommand(livingRoomLight);
+
+        RemoteControl remote = new RemoteControl();
+        remote.setCommand(turnOn);
+        remote.pressButton();
+
+        remote.setCommand(turnOff);
+        remote.pressButton();
+    }
+}
